108d53c0ab01a04813d9bd4f722f9edc0227c5df
unknown <kadajgarcia@gmail.com>
2021-10-04 11:43:15 -0300

    fr_US_ES_fixed_two

--- pages/Context/langContext3.tsx
+++ pages/Context/langContext3.tsx
@@ -1,18 +1,14 @@
-import React, { createContext, useState, useMemo } from "react";
+import React, { createContext, useState, useMemo, useContext } from "react";
 import MensajesEsp from "../Lang/es-CL.json";
 import MensajesEng from "../Lang/en-US.json";
 import MensajesFr from "../Lang/fr-FR.json";
-import { IntlProvider, IntlShape } from "react-intl";
+import { IntlProvider } from "react-intl";
 
 const langContext = createContext("");
 
-const LangProvider = ({ children }: any) => {
+export function LangProvider(children: any) {
   const [locale, setLocale] = useState("es-CL");
   const [mensajes, setMensajes] = useState(MensajesEsp);
-
-  interface Props {
-    intl: IntlShape;
-  }
 
   const setLenguaje = (lenguaje: any) => {
     switch (lenguaje) {
@@ -33,13 +29,25 @@ const LangProvider = ({ children }: any) => {
     }
   };
 
+  const value = useMemo(() => {
+    return {
+      setLenguaje,
+    };
+  }, [locale, mensajes]);
+
   return (
-    <langContext.Provider value={{ setLenguaje: setLenguaje }}>
+    <langContext.Provider value={value}>
       <IntlProvider locale={locale} messages={mensajes}>
         {children}
       </IntlProvider>
     </langContext.Provider>
   );
-};
+}
 
-export { LangProvider, langContext };
+export function Menu() {
+  const context = useContext(langContext);
+  if (!context) {
+    throw new Error("No esta dentro del Proveedor");
+  }
+  return context;
+}
