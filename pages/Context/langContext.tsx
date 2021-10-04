520c67598f9a535320eba190c3bc4bb1dcec7de4
unknown <kadajgarcia@gmail.com>
2021-10-04 13:35:58 -0300

    fr_US_ES_fixed_thisisit

--- pages/Context/langContext.tsx
+++ pages/Context/langContext.tsx
@@ -10,7 +10,7 @@ const LangProvider = ({ children }: any) => {
   const [locale, setLocale] = useState("es-CL");
   const [mensajes, setMensajes] = useState(MensajesEsp);
 
-  const setLenguaje = (lenguaje: any) => {
+  const setLenguaje = (lenguaje: string) => {
     switch (lenguaje) {
       case "es-CL":
         setLocale("es-CL");
@@ -30,7 +30,7 @@ const LangProvider = ({ children }: any) => {
   };
 
   return (
-    <langContext.Provider value={{ setLenguaje: setLenguaje }}>
+    <langContext.Provider value={{ setLenguaje }}>
       <IntlProvider locale={locale} messages={mensajes}>
         {children}
       </IntlProvider>
